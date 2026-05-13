944ffbc9c84e5e445d88a01772dd2192f6e3ba9a
Floris den Hartog <floris.denhartog2308@gmail.com>
2026-05-13 16:25:37 +0200

    Changed some plots and added print statements to track progress. Also added a method to stop the wave generator for cleaner scope data.

--- analysis.py
+++ analysis.py
@@ -6,10 +6,12 @@ import time
 
 start_time = time.time()
 
+
 def import_data(file_path):
     # Assuming the data is in CSV format with columns: Time, Voltage
     data = pd.read_csv(file_path, skiprows=9)
-    return data['Time (s)'], data['Channel 1 (V)']
+    return data["Time (s)"], data["Channel 1 (V)"]
+
 
 i = "0001"
 
@@ -17,32 +19,36 @@ integral_list = []
 
 while os.path.exists(f"Data/acq{i}.csv"):
     times, voltage = import_data(f"Data/acq{i}.csv")
-    
+
     integral = np.trapezoid(voltage, times)
-    
-    print(f"Integral for acq{i}.csv: {integral:.4f}")
-    
+
+    print(f"Integral for acq{i}.csv: {integral:.4f}", end="\r")
+
     i = str(int(i) + 1).zfill(4)
-    
+
     integral_list.append(integral)
-    
+
     # plt.figure(figsize=(10, 6))
     # plt.plot(times, voltage)
     # plt.title(f"Voltage vs Time for acq{i}.csv")
     # plt.xlabel("Time (s)")
     # plt.ylabel("Voltage (V)")
     # plt.savefig(f"Figures/acq/acq{i}.png")
-    
+
 
 plt.figure(figsize=(10, 6))
-plt.hist(integral_list, bins=int(np.sqrt(len(integral_list))), color='blue', edgecolor='black', density=True)
+counts, bins = np.histogram(
+    integral_list,
+    bins=int(np.sqrt(len(integral_list))),
+    density=True,
+)
+plt.stairs(counts, bins, edgecolor="black", alpha=0.7)
+plt.xlabel("Integral of Voltage over Time")
+plt.ylabel("Density")
 
 end_time = time.time()
 
-plt.title('Histogram of Integrals')
+plt.title("Histogram of Integrals")
 plt.savefig("Figures/integral_histogram.png")
 
 print(f"Processed {len(integral_list)} files in {end_time - start_time:.2f} seconds.")
-
-print("No more files to process.")
-
