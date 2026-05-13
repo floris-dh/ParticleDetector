c222c962b72339ded0b612c4c75141a7f97a86cb
Floris den Hartog <floris.denhartog2308@gmail.com>
2026-05-13 16:11:10 +0200

    initial commit

---
+++ analysis.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import time
+
+start_time = time.time()
+
+def import_data(file_path):
+    # Assuming the data is in CSV format with columns: Time, Voltage
+    data = pd.read_csv(file_path, skiprows=9)
+    return data['Time (s)'], data['Channel 1 (V)']
+
+i = "0001"
+
+integral_list = []
+
+while os.path.exists(f"Data/acq{i}.csv"):
+    times, voltage = import_data(f"Data/acq{i}.csv")
+    
+    integral = np.trapezoid(voltage, times)
+    
+    print(f"Integral for acq{i}.csv: {integral:.4f}")
+    
+    i = str(int(i) + 1).zfill(4)
+    
+    integral_list.append(integral)
+    
+    # plt.figure(figsize=(10, 6))
+    # plt.plot(times, voltage)
+    # plt.title(f"Voltage vs Time for acq{i}.csv")
+    # plt.xlabel("Time (s)")
+    # plt.ylabel("Voltage (V)")
+    # plt.savefig(f"Figures/acq/acq{i}.png")
+    
+
+plt.figure(figsize=(10, 6))
+plt.hist(integral_list, bins=int(np.sqrt(len(integral_list))), color='blue', edgecolor='black', density=True)
+
+end_time = time.time()
+
+plt.title('Histogram of Integrals')
+plt.savefig("Figures/integral_histogram.png")
+
+print(f"Processed {len(integral_list)} files in {end_time - start_time:.2f} seconds.")
+
+print("No more files to process.")
+
